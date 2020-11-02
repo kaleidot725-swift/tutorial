feea72a1c5d91d9c8ebe634833bc0161bb49d16a
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-02 23:26:59 +0900

    feat: let と var の使い方

--- hello_world/hello_world/hello_world/main.swift
+++ hello_world/hello_world/hello_world/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
+// No1 Hello, World
+print("No1")
 print("Hello, World!")
 
+// No2 変数の定義
+print("No2")
+let letValue = "test"
+print("let:" + letValue)
+
+var varValue = "test"
+print("var before: " + varValue)
+
+varValue = "update"
+print("var after: " + letValue)
