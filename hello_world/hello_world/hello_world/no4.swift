749e4058ac7108b877b427e05a78b61d3902aa44
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-03 21:41:39 +0900

    feat: variable compairing

---
+++ hello_world/hello_world/hello_world/no4.swift
@@ -0,0 +1,24 @@
+//
+//  no4.swift
+//  hello_world
+//
+//  Created by kaleidot725 on 2020/11/03.
+//
+
+import Foundation
+
+func no4() {
+    // No4 比較について
+    
+    // 整数型ならば異なる型でも比較できる
+    let m: UInt = 1000
+    let n: Int8 = 64
+    print(m == n)
+    print(m > n)
+    
+    // 実数型だと異なる型だと比較できない
+    let d: Double = 1.2
+    let f: Float = 1.4
+    // print(d == f) -> Binary operator '==' cannot be applied to operands of type 'Double' and 'Float'
+    // print(d > f)  -> Binary operator '>' cannot be applied to operands of type 'Double' and 'Float'
+}
