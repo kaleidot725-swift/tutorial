77388192b7b2907d8c2169a23dca4e05ce3e2a59
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-02-16 13:39:28 +0900

    feat: update struct property

--- hello_world/hello_world/hello_world/episode03/24_struct_property.swift
+++ hello_world/hello_world/hello_world/episode03/24_struct_property.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+var serialNumber = 2127
+
 func no24() {
     // 定数や変数が機能を提供するプロパティを格納型プロパティ
     // 値の参照と更新の機能を手続きで構成するプロパティを計算型プロパティと呼ぶ
@@ -51,4 +53,88 @@ func no24() {
     DateWithString.longFormat = true
     let b = DateWithString(2025, 1, 21)
     print(b.string)
+    
+    // 計算型プロパティで Getter や Setter を定義できる。
+    // 次のように Getter を定義できる、 Getter プロパティを呼び出したときに値を再評価する処理がかける
+    struct LCD  {
+        struct Size { var width, height: Int }
+        
+        static var stdHeight = 1080
+        static var stdWidth = 1920
+        static var stdSize : Size {
+            get {
+                return Size(width: stdWidth, height: stdHeight)
+            }
+        }
+        
+        static func sn() -> Int { serialNumber += 1; return serialNumber }
+        let size: Size
+        let serial = "CZ:" + String(LCD.sn())
+        
+        init(size: Size = LCD.stdSize) { self.size = size }
+        
+        func show() {
+            print(serial, "(\(size.width)x\(size.height))")
+        }
+    }
+    
+    let display1 = LCD(size: LCD.Size(width: 800, height: 600))
+    display1.show()
+    
+    LCD.stdHeight = 1200
+    let display2 = LCD()
+    display2.show()
+    
+    LCD.stdWidth = 2560
+    let display3 = LCD()
+    display3.show()
+    
+    // 次のように Getter や Setter を定義できる、 Setter では新たに指定された値を受け取る仮引数を定義できる。
+    // もし仮引数の定義が省略された場合には newValue という名前でアタラに指定された値を利用できる。
+    struct Ounce {
+        var mL:Double = 0.0
+        static let ounceUS = 29.5735
+        init(ounce:Double) {
+            self.ounce = ounce
+        }
+        
+        var ounce: Double {
+            get { mL / Ounce.ounceUS }
+            set { mL = newValue * Ounce.ounceUS  }
+        }
+    }
+    
+    var c = Ounce(ounce: 2.0)
+    print(c.mL)
+    c.ounce += 8.0
+    print(c.ounce)
+    
+    // プロパティオブザーバ
+    // newValue という oldValue という参照可能です
+    struct Stock {
+        let buyingPrice: Int
+        var high = false
+        var count = 0
+        
+        init(price:Int) {
+            buyingPrice = price
+            self.price = price
+        }
+        
+        var price:Int {
+            willSet {
+                count += 1
+                high = newValue > buyingPrice
+            }
+            didSet {
+                print("\(oldValue) => \(price)")
+            }
+        }
+    }
+    
+    var st = Stock(price:400)
+    st.price = 410
+    st.price = 380
+    st.price = 430
+    print("\(st.count), \(st.high)")
 }
