38b8702f17d4263ddf9e962c1cd010508f25e5d1
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-03-31 09:25:52 +0900

    feat: update optional func

--- hello_world/hello_world/hello_world/episode04/29_optional_func.swift
+++ hello_world/hello_world/hello_world/episode04/29_optional_func.swift
@@ -13,9 +13,17 @@ func no29() {
     
     let name2 = nickname(nil, age:20)
     print(name2)
+    
+    var w: String? = ""
+    makeNil(&w)
+    print(w ?? "nil")
 }
 
 func nickname(_ name: String?, age: Int) -> String {
     let s = name ?? "名無し"
     return "浪速の" + s + "(\(age)歳)"
+}
+
+func makeNil(_ p: inout String?) {
+    if let s = p, s == "" { p = nil }
 }
