ecbf760bb53e48123184ac25c17a3a4a3a2024ef
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-07-10 15:21:38 +0900

    feat: update equatable

--- hello_world/hello_world/hello_world/episode05/37_equatable.swift
+++ hello_world/hello_world/hello_world/episode05/37_equatable.swift
@@ -12,6 +12,27 @@ func no37() {
     let a = LabeledPoint(label:"A", x:mx, y:7)
     var b = LabeledPoint(label:"B", x:10, y: mx - 3)
     print(a != b)
+    
+    let personA = Person(firstName: "A", lastName: "B", age: 1)
+    let personB = Person(firstName: "B", lastName: "A", age: 2)
+    let personC = Person(firstName: "A", lastName: "B", age: 1)
+    
+    print(personA == personB) // false
+    print(personA == personC) // true
+    
+    let animalA = Animal(name: "Dog", age: 3)
+    let animalB = Animal(name: "Cat", age: 4)
+    let animalC = Animal(name: "Dog", age: 3)
+    
+    print(animalA == animalB) // false
+    print(animalA == animalC) // true
+    
+    let blood1 = Blood.a
+    let blood2 = Blood.b
+    let blood3 = Blood.a
+    
+    print(blood1 == blood2) // false
+    print(blood1 == blood3) // true
 }
 
 protocol EqVector: Equatable {
@@ -46,10 +67,39 @@ struct ShopOnMap: EqVector, CustomStringConvertible {
     }
 }
 
-//  Equatable の定義
-//   - 二項演算子 == を定義している、これにて比較ができるようになっているらしい
-//   - 二項演算子 != は定義していない、これは == を否定した結果とイコールになるので定義していなことらしい
-//
-//  public protocol Equatable {
-//      static func == (lhs: Self, rhs: Self) -> Bool
-//  }
+struct Person: Equatable {
+    let firstName: String
+    let lastName: String
+    let age: Int
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    static func ==(lhs: Person, rhs: Person) -> Bool {
+        return lhs.firstName == rhs.firstName && lhs.lastName == rhs.lastName && lhs.age == rhs.age
+    }
+}
+
+class Animal: Equatable {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    static func ==(lhs: Animal, rhs: Animal) -> Bool {
+        return lhs.name == rhs.name && lhs.age == rhs.age
+    }
+}
+
+enum Blood: Equatable {
+    case ab
+    case a
+    case b
+    case o
+}
